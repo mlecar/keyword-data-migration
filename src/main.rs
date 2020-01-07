7572231f0ffea2625ba1cb7d3dd873bdc1f5d673
Marcelo Lecar <marcelo.lecar@gmail.com>
2020-01-07 06:46:10 +0100

    preparing request

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,39 @@
 extern crate config;
 
+#[macro_use]
+extern crate serde_json;
+
 use reqwest::StatusCode;
+use config::Config;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-
-    let mut settings = config::Config::default();
+    // read configurations
+    let mut settings: Config = config::Config::default();
     settings.merge(config::File::with_name("settings")).unwrap();
 
     let keyword_url:String = settings.get::<String>("keywordurl").unwrap();
-
     println!("Settings {:?}", &keyword_url);
 
-    let resp = reqwest::blocking::get(&keyword_url)?;
+    // prepare params
+    let mut keyword_vec: Vec<i64> = Vec::new();
+    for x in 0..10 {
+        keyword_vec.push(x);
+    }
+
+    let item = json!({
+        "keyword_id": &keyword_vec,
+        "ignore_errors": true
+    });
+
+    // make request
+    let client = reqwest::blocking::Client::new();
+
+    let resp = client.get(&keyword_url).json(&item).send()?;
 
     match resp.status() {
         StatusCode::OK => println!("{:#?}", resp.text()?),
         s => {
-            println!("Received response status: {:?}", s);
+            println!("Received response status: {:?}, body {:?}", s, resp.text());
         },
     };
 
