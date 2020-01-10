0e8d2b8e789edb436d7e09759a91622e7ca25498
Marcelo Lecar <m.lecar@searchmetrics.com>
2020-01-10 09:53:58 +0100

    extracting database configurations

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ use reqwest::StatusCode;
 use config::Config;
 use serde_json::Value;
 use serde::{Deserialize};
-use log::{info, warn, Level};
-use mysql::Pool;
+use log::{info, warn, error, Level};
+use mysql::{Pool, Error};
 
 #[derive(Debug, Deserialize)]
 struct KeywordResult {
@@ -37,7 +37,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let database_password = settings.get::<String>("database_password").unwrap();
     let database_name = settings.get::<String>("database_name").unwrap();
 
-    let mut increment = 2000;
+    let increment = 2000;
 
     // client for request
     let client = reqwest::blocking::Client::new();
@@ -76,8 +76,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 info!("valid_keywords {:?}", valid_keywords);
                 info!("empty_keywords {:?}", empty_keywords);
 
-                save_keyword(&valid_keywords, &pool);
-                save_unused_keyword_ids(&empty_keywords, &pool);
+                if let Err(_err) = save_keyword(&valid_keywords, &pool) {
+                    error!("Could not save keywords {:?}", _err);
+                }
+                if let Err(_err) = save_unused_keyword_ids(&empty_keywords, &pool){
+                    error!("Could not save unused keywords {:?}", _err);
+                }
             },
             s => {
                 warn!("Received response status: {:?}, body {:?}", s, resp.text());
@@ -91,7 +95,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn save_keyword(valid_keywords: &Vec<&Value>, pool:&Pool) {
+fn save_keyword(valid_keywords: &Vec<&Value>, pool:&Pool) -> Result<(), Error> {
     // stringprefix r means it will be treated as a raw string
     for mut stmt in pool.prepare(r"INSERT IGNORE INTO keyword_test (id, keyword) VALUES (:keyword_id, :keyword)").into_iter() {
         for x in valid_keywords {
@@ -103,16 +107,16 @@ fn save_keyword(valid_keywords: &Vec<&Value>, pool:&Pool) {
                         }).unwrap();
         }
     }
+    Ok(())
 }
 
-fn save_unused_keyword_ids(empty_keywords: &Vec<&Value>, pool:&Pool) {
+fn save_unused_keyword_ids(empty_keywords: &Vec<&Value>, pool:&Pool) -> Result<(), Error>{
     // stringprefix r means it will be treated as a raw string
     for mut stmt in pool.prepare(r"INSERT IGNORE INTO unused_keyword_id_test (id) VALUES (:keyword_id)").into_iter() {
         for x in empty_keywords {
             let keyword_id = x["keyword_id"].as_i64().unwrap();
-            stmt.execute(params! {
-                            "keyword_id" => keyword_id,
-                        }).unwrap();
+            stmt.execute(params! {"keyword_id" => keyword_id}).unwrap();
         }
     }
+    Ok(())
 }
